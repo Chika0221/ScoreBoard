044c089845d6adf9eb510d330f700955b7ed67a0
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-10-20 11:33:38 +0900

    fix: import修正

--- lib/pages/display_page/rank_card.dart
+++ lib/pages/display_page/rank_card.dart
@@ -1,10 +1,7 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Package imports:
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 // Project imports:
 import 'package:m_score_board/models/team.dart';
 import 'package:m_score_board/theme.dart';
